754c07f9070b0c9c7e1cdd55348107e9ee999eb0
Matheus Batista <matheusifro2020@gmail.com>
2025-08-12 21:32:55 -0400

    chore: adding formspree api to handle the contact section

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active')
 }
+
+document.getElementById("contactForm").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const status = document.getElementById("formStatus");
+
+    status.textContent = "Sending message...";
+
+    try {
+        const response = await fetch("https://formspree.io/f/mblkwzll", {
+            method: "POST",
+            body: new FormData(form),
+            headers: { 'Accept': 'application/json' }
+        });
+
+        if (response.ok) {
+            alert("Message sent successfully!")
+            form.reset();
+        } else {
+            alert("Message not sent. Try again!")
+        }
+    } catch (error) {
+        alert("Network error. Please try again later.");
+    }
+});
